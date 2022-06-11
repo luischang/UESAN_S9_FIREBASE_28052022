e671527dda0226a40237b399bd8747faa3226cc7
Luis Chang <luis.chang@iugo.com.uy>
2022-06-11 12:04:12 -0500

    Register course

--- app/src/main/java/dev/lchang/uesan_s9_firebase/ui/fragments/RegistroFragment.kt
+++ app/src/main/java/dev/lchang/uesan_s9_firebase/ui/fragments/RegistroFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.FirebaseFirestore
 import dev.lchang.uesan_s9_firebase.R
+import dev.lchang.uesan_s9_firebase.ui.fragments.models.CourseModel
+import java.util.*
 
 
 class RegistroFragment : Fragment() {
@@ -15,6 +21,29 @@ class RegistroFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registro, container, false)
+        val view: View =  inflater.inflate(R.layout.fragment_registro, container, false)
+        val txtCourse: EditText = view.findViewById(R.id.txtCourse)
+        val txtScore: EditText = view.findViewById(R.id.txtScore)
+        val btnSave: Button = view.findViewById(R.id.btnSave)
+        val db = FirebaseFirestore.getInstance()
+
+        btnSave.setOnClickListener {
+            val curso = txtCourse.text.toString()
+            val nota = txtScore.text.toString()
+
+            val nuevoCurso = CourseModel(curso,nota)
+            val id: UUID = UUID.randomUUID()
+
+            db.collection("courses")
+                .document(id.toString())
+                .set(nuevoCurso)
+                .addOnSuccessListener {
+                    Snackbar.make(view,"Registro exitoso",Snackbar.LENGTH_LONG).show()
+                }.addOnFailureListener {
+                    Snackbar.make(view,"Ocurrió un error al registrar el curso",Snackbar.LENGTH_LONG).show()
+                }
+
+        }
+        return view
     }
 }
---
+++ app/src/main/java/dev/lchang/uesan_s9_firebase/ui/fragments/models/CourseModel.kt
@@ -0,0 +1,6 @@
+package dev.lchang.uesan_s9_firebase.ui.fragments.models
+
+data class CourseModel(
+    val description: String,
+    val score: String
+)
